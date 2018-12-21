0708edb8eebd611f007ace9afc5f0b3314d2eed9
jose david rincon angeles <angelnmara@hotmail.com>
2018-12-21 17:03:01 -0600

    delete y put

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient
+const BSON = require('bson');
 
 var db
 
@@ -14,6 +15,7 @@ MongoClient.connect('mongodb://dave:maradr1980@ds127101.mlab.com:27101/lamarrull
   console.log('se conecta a la base de datos')
 
   app.use(bodyParser.urlencoded({extended: true}))
+  app.use(bodyParser.json());
 
   console.log('se agrega body parser')
 
@@ -25,26 +27,84 @@ MongoClient.connect('mongodb://dave:maradr1980@ds127101.mlab.com:27101/lamarrull
     res.sendFile(__dirname + '/index.html')
   })
 
-  app.get('/quotes', (req, res) => {
-    var cursor = db.collection('quotes').find().toArray(function(err, result){
-        console.log(result)
-    })
-    // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
-    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
-  })
-
   console.log('se agrega get')
 
+  app.get('/quotes', (req, res) => {    
+    try{
+      var cursor = db.collection('quotes').find().toArray(function(err, result){
+        console.log(result)
+        res.json(result)
+    })
+    }catch(e){
+      res.json('{error:"Error: "' + e + '}')
+      console.log(e);
+    }        
+  })
+
+  console.log('se agrega get by id')
+
+  app.get('/quotes/:id', (req, res) => {
+    try{
+      var id = req.params.id;          
+      console.log(id)
+      var cursor = db.collection('quotes').find({_id:BSON.ObjectId(id)}).toArray(function(err, result){
+        console.log(result)
+        res.json(result)
+      })
+    }catch(e){
+      res.json('{error:"Error: "' + e + '}')
+      console.log(e);
+    }        
+  })
+
+  console.log('se agrega post')
+
   app.post('/quotes', (req, res) => {
-    db.collection('quotes').save(req.body, (err, result) => {
+    try{
+      db.collection('quotes').save(req.body, (err, result) => {
         if (err) return console.log(err)
     
         console.log('saved to database')
         res.redirect('/')
       })
+    }catch(e){
+      res.json('{error:"Error: "' + e + '}')
+      console.log(e);
+    }    
   })
 
-  console.log('se agrega post')
+  console.log('agrega update')
+
+  app.put('/quotes/:id', (req, res)=>{
+    try{
+      var id = req.params.id;      
+      console.log(id)
+      console.log(req.body)      
+      db.collection('quotes').updateOne({_id:BSON.ObjectId(id)}, {$set:req.body}, function(err, result){
+        console.log(result)
+        res.json(result)
+      })      
+    }catch(e){
+      res.json('{error:"Error: "' + e + '}')
+      console.log(e);
+    }
+  })
+
+  console.log('se agrega delete')
+
+  app.delete('/quotes/:id', (req, res) => {
+    try{
+      var id = req.params.id;
+      console.log(id)
+      var cursor = db.collection('quotes').deleteOne({_id:BSON.ObjectId(id)}, function(err, result){
+        console.log(result)
+        res.json(result)
+    })
+    }catch(e){
+      res.json('{error:"Error: "' + e + '}')
+      console.log(e);
+    }          
+  })
 
 })
 
